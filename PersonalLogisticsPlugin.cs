67af645853e2565a7148fc957ed3c6dd558e6e56
Matt Semar <mattsemar@gmail.com>
2022-01-06 22:43:32 -0600

    Version 2.5.0

--- PersonalLogisticsPlugin.cs
+++ PersonalLogisticsPlugin.cs
@@ -36,7 +36,7 @@ namespace PersonalLogistics
     {
         private const string PluginGuid = "semarware.dysonsphereprogram.PersonalLogistics";
         private const string PluginName = "PersonalLogistics";
-        private const string PluginVersion = "2.4.0";
+        private const string PluginVersion = "2.5.0";
         private const float InventorySyncInterval = 4.5f;
         private static readonly int VERSION = 2;
 
